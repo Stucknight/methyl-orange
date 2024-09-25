a089ad97151024bde3d1d14b04a06126d46c5a48
Stucknight <chengweifan2020@gmail.com>
2024-09-25 17:55:36 +0800

    test app

--- app/page.js
+++ app/page.js
@@ -1,95 +1,260 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+import "@/public/globals.css"
+import Renderer from '@/public/render.wgsl';
+import Simulation from '@/public/compute.wgsl';
+import { useEffect } from 'react';
+
+const WEBGPU_NOT_SUPPORTED = 404;
+const RENDER_LOOP_INTERVAL = 0;
+
+async function initializeWebGPU(canvas) {
+    const GPU = navigator.gpu;
+
+    if (!GPU) throw new Error('WebGPU is not supported on this browser.', { cause: WEBGPU_NOT_SUPPORTED });
+
+    const adapter = await GPU.requestAdapter({ powerPreference: 'high-performance', forceFallbackAdapter: false });
+
+    if (!adapter) throw new Error('No appropriate GPUAdapter found.');
+
+    const context = canvas.getContext('webgpu');
+
+    if (!context) throw new Error('Failed to initialize WebGPU context.');
+
+    const device = await adapter.requestDevice();
+
+    const format = GPU.getPreferredCanvasFormat();
+
+    context.configure({ device, format });
+
+    return { context, device, format };
+}
+
+function createComputePipeline(layout, device) {
+    const simulationShaderModule = device.createShaderModule({ label: 'Simulation Shader', code: Simulation });
+
+    return device.createComputePipeline({
+        label: 'Simulation Pipeline',
+        layout,
+        compute: {
+            module: simulationShaderModule,
+            entryPoint: 'mainCompute'
+        }
+    });
+}
+
+function createGridBindGroups(device, size) {
+    const uniformArray = new Float32Array([size, size]);
+    const uniformBuffer = device.createBuffer({
+        label: 'Grid Uniforms',
+        size: uniformArray.byteLength,
+        usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST
+    });
+
+    device.queue.writeBuffer(uniformBuffer, 0, uniformArray);
+
+    const storageArray = new Uint32Array(size * size);
+    const storageBuffers = [
+        device.createBuffer({
+            label: 'Cell State A',
+            size: storageArray.byteLength,
+            usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST
+        }),
+        device.createBuffer({
+            label: 'Cell State B',
+            size: storageArray.byteLength,
+            usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST
+        })
+    ];
+
+    for (let i = 0; i < storageArray.length; i++) {
+        storageArray[i] = +(Math.random() > 0.6);
+    }
+
+    device.queue.writeBuffer(storageBuffers[0], 0, storageArray);
+
+    const bindGroupLayout = device.createBindGroupLayout({
+        label: 'Cell Bind Group Layout',
+        entries: [
+            {
+                binding: 0,
+                buffer: { type: 'uniform' },
+                visibility: GPUShaderStage.COMPUTE | GPUShaderStage.VERTEX | GPUShaderStage.FRAGMENT
+            },
+            {
+                binding: 1,
+                buffer: { type: 'read-only-storage' },
+                visibility: GPUShaderStage.COMPUTE | GPUShaderStage.VERTEX
+            },
+            {
+                binding: 2,
+                buffer: { type: 'storage' },
+                visibility: GPUShaderStage.COMPUTE
+            }
+        ]
+    });
+
+    const bindGroups = [
+        device.createBindGroup({
+            label: 'Cell Bind Group A',
+            layout: bindGroupLayout,
+            entries: [
+                { binding: 0, resource: { buffer: uniformBuffer } },
+                { binding: 1, resource: { buffer: storageBuffers[0] } },
+                { binding: 2, resource: { buffer: storageBuffers[1] } }
+            ]
+        }),
+        device.createBindGroup({
+            label: 'Cell Bind Group B',
+            layout: bindGroupLayout,
+            entries: [
+                { binding: 0, resource: { buffer: uniformBuffer } },
+                { binding: 1, resource: { buffer: storageBuffers[1] } },
+                { binding: 2, resource: { buffer: storageBuffers[0] } }
+            ]
+        })
+    ];
+
+    return {
+        layout: bindGroupLayout,
+        groups: bindGroups
+    };
+}
+
+function createRenderPipeline(device, format, layout) {
+    const vertices = new Float32Array([
+        -0.8, 0.8,
+        0.8, 0.8,
+        -0.8, -0.8,
+        -0.8, -0.8,
+        0.8, -0.8,
+        0.8, 0.8
+    ]);
+
+    const vertexBuffer = device.createBuffer({
+        label: 'Cell Vertices',
+        size: vertices.byteLength,
+        usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST
+    });
+
+    device.queue.writeBuffer(vertexBuffer, 0, vertices);
+
+    const pipelineLayout = device.createPipelineLayout({
+        label: 'Cell Pipeline Layout',
+        bindGroupLayouts: [layout],
+    });
+
+    const vertexBufferLayout = {
+        arrayStride: 8,
+        attributes: [{
+            format: 'float32x2',
+            shaderLocation: 0,
+            offset: 0
+        }]
+    };
+
+    const cellShaderModule = device.createShaderModule({ label: 'Cell Shader', code: Renderer });
+
+    const cellPipeline = device.createRenderPipeline({
+        label: 'Cell Pipeline',
+        layout: pipelineLayout,
+        vertex: {
+            buffers: [vertexBufferLayout],
+            module: cellShaderModule,
+            entryPoint: 'mainVert'
+        },
+        fragment: {
+            module: cellShaderModule,
+            entryPoint: 'mainFrag',
+            targets: [{ format }]
+        }
+    });
+
+    return {
+        vertices: vertices.length / 2,
+        pipeline: cellPipeline,
+        layout: pipelineLayout,
+        buffer: vertexBuffer
+    };
+}
+
+function createRenderPass(pipelines, context, groups, workgroupSize, device, buffer, instances, vertices, gridSize, step) {
+    const commandEncoder = device.createCommandEncoder();
+
+    const computePass = commandEncoder.beginComputePass();
+
+    computePass.setPipeline(pipelines[0]);
+    computePass.setBindGroup(0, groups[step % 2]);
+
+    const workgroupCount = Math.ceil(gridSize / workgroupSize);
+
+    computePass.dispatchWorkgroups(workgroupCount, workgroupCount);
+    computePass.end();
+
+    const renderPass = commandEncoder.beginRenderPass({
+        colorAttachments: [{
+            view: context.getCurrentTexture().createView(),
+            clearValue: [0, 0, 0.4, 1],
+            storeOp: 'store',
+            loadOp: 'clear'
+        }]
+    });
+
+    renderPass.setPipeline(pipelines[1]);
+    renderPass.setVertexBuffer(0, buffer);
+    renderPass.setBindGroup(0, groups[++step % 2]);
+    renderPass.draw(vertices, instances);
+    renderPass.end();
+
+    device.queue.submit([commandEncoder.finish()]);
+
+    return step;
+}
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    useEffect(() => {
+        initializeWebGPU(document.getElementsByTagName('canvas')[0])
+            .then(({ context, device, format }) => {
+                let step = 0,
+                    size = 2 ** 12,
+                    instances = size * size,
+                    lastRender = performance.now();
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+                const { layout: bindGroupLayout, groups } = createGridBindGroups(device, size);
+                const { pipeline: renderPipeline, layout: pipelineLayout, buffer, vertices } = createRenderPipeline(device, format, bindGroupLayout);
+                const computePipeline = createComputePipeline(pipelineLayout, device);
+
+                const runSimulation = (time) => {
+                    requestAnimationFrame(runSimulation);
+                    if (time - lastRender < RENDER_LOOP_INTERVAL) return;
+
+                    step = createRenderPass(
+                        [computePipeline, renderPipeline],
+                        context,
+                        groups,
+                        8,
+                        device,
+                        buffer,
+                        instances,
+                        vertices,
+                        size,
+                        step
+                    );
+
+                    lastRender = time;
+                };
+
+                requestAnimationFrame(runSimulation);
+            })
+            .catch(error =>
+                error.cause === WEBGPU_NOT_SUPPORTED
+                    ? alert(error.message)
+                    : console.error(error)
+            );
+    }, []);
+
+    return (
+        <div className="text-center" id="container">
+            <canvas id="canvas" width="1920" height="1080"/>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    );
 }
---
+++ public/compute.wgsl
@@ -0,0 +1,50 @@
+// Uniform buffer for the grid size.
+@group(0) @binding(0) var<uniform> grid: vec2f;
+
+// Exposed cell input and output states as storage buffers.
+@group(0) @binding(1) var<storage> cellStateIn: array<u32>; // read-only
+@group(0) @binding(2) var<storage, read_write> cellStateOut: array<u32>;
+
+// Mapped cell index into a linear storage array.
+fn cellIndex(cell: vec2u) -> u32 {
+  // Cells on the edge of the grid will treat cells on the opposite edge as their neighbors,
+  // creating a kind of wrap-around effect in order to prevent out-of-bounds accesses.
+  return (cell.x % u32(grid.x)) + (cell.y % u32(grid.y)) * u32(grid.x);
+}
+
+// Cell state at the given grid coordinates.
+fn cellActive(x: u32, y: u32) -> u32 {
+  return cellStateIn[cellIndex(vec2(x, y))];
+}
+
+@compute
+@workgroup_size(16, 16)
+fn mainCompute(
+  // Three-dimensional vector of unsigned integers holding the index
+  // of the current call from the grid of shader invocations.
+  @builtin(global_invocation_id) cell: vec3u
+) {
+  // Amount of active neighbors of the current cell.
+  let activeNeighbors =
+    cellActive(cell.x + 1, cell.y + 1) +
+    cellActive(cell.x + 1, cell.y    ) +
+    cellActive(cell.x + 1, cell.y - 1) +
+    cellActive(cell.x    , cell.y - 1) +
+    cellActive(cell.x - 1, cell.y - 1) +
+    cellActive(cell.x - 1, cell.y    ) +
+    cellActive(cell.x - 1, cell.y + 1) +
+    cellActive(cell.x    , cell.y + 1);
+
+  let i = cellIndex(cell.xy);
+
+  switch activeNeighbors
+  {
+    // Cells with two neighbors stay active.
+    case 2: { cellStateOut[i] = cellStateIn[i]; }
+    // Cells with exactly three neighbors become active.
+    case 3: { cellStateOut[i] = 1; }
+    // Cells with fewer than two neighbors or with
+    // more than three neighbors become inactive.
+    default: { cellStateOut[i] = 0; }
+  }
+}
